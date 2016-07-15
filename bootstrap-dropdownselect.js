bb90b36ffa178c1bf59e5b5a522d898b0a9b4de5
João <joaogabrielv@gmail.com>
2016-07-15 17:57:53 -0300

    Implement keybuffer cache clear timeout and arrow keys support

--- bootstrap-dropdownselect.js
+++ bootstrap-dropdownselect.js
@@ -3,10 +3,24 @@
 //Adds ability to type letters in with Bootstrap dropdown open, jumping
 //to items starting with letters pressed
 
+var clearKeybufferInterval;
+
+//clear data-keypressed attribute after x seconds without pressing a key
+function setClearKeypressedTimeout(x, dropdownName){
+  clearInterval(clearKeybufferInterval);
+  clearKeybufferInterval = window.setInterval(function(){
+     $('#' + dropdownName).attr('data-keyspressed', '');
+  },x);
+}
+
+
 function SetupDropdownSelect(buttonName, dropdownName) {
     //handle the user pressing keys while inside the dropdown by highlighting ones that match the keys
     //they press. the string of keys they've pressed resets whenenver a dropdown box gets focus
     $('#' + buttonName + ', #' + dropdownName).keypress(function (e) {
+
+        setClearKeypressedTimeout(1500, dropdownName);
+
         if (!$('#' + dropdownName).parent().hasClass('open')) {
             $('#' + buttonName).click();
         }
@@ -20,32 +34,53 @@ function SetupDropdownSelect(buttonName, dropdownName) {
         $('#' + dropdownName).attr('data-keyspressed', previousKeysPressed + keyPressed);
 
         //loop through the dropdown and find the item that matches what the user has typed in so far
-        $('#' + dropdownName + ' li a').each(function () {
-            if ($(this).text().substring(0, currentKeysPressed.length).toLowerCase() == currentKeysPressed) {
+        $($('#' + dropdownName + ' li a').get().reverse()).each(function () {
+            if ($(this).text().substring(0, currentKeysPressed.length).toLowerCase() === currentKeysPressed) {
                 $(this).focus();
             }
         });
+
+        // Disable scrolldown when press spacebar
+        if (e.which === 32) {
+            e.preventDefault();
+            return;
+        }
+
     });
 
-    //handle key actions "enter", "tab", and "space" from interfering with the dropdown
+    //handle key actions "enter", "tab", arrows and "space" from interfering with the dropdown
     $('#' + buttonName + ', #' + dropdownName).keydown(function (e) {
 
+        // Up arrow
+        if (e.which === 38){
+          $('#' + dropdownName + ' li a:focus').parent().prev().find('a').focus();
+          e.preventDefault();
+          return;
+        }
+
+        // Down arrow
+        else if (e.which === 40){
+          $('#' + dropdownName + ' li a:focus').parent().next().find('a').focus();
+          e.preventDefault();
+          return;
+        }
+
         //let the tab presses pass through
-        if (e.which == 9) {
+        if (e.which === 9) {
             return;
         }
 
         //if we are pressing enter or space and the dropdown is closed, open it
         //catch the key and don't let the browser do anything afterwards
-        if ((e.which == 13 || e.which == 32) && !$('#' + dropdownName).parent().hasClass('open')) {
+        if ((e.which === 13 || e.which === 32) && !$('#' + dropdownName).parent().hasClass('open')) {
             $('#' + buttonName).click();
             return false;
         }
 
         //if we are pressing enter and the dropdown is open, click the item that has focus to select it
         //let the key pass through
-        else if (e.which == 13) {
-            $('#' + dropdownName + ' li:focus').click();
+        else if (e.which === 13) {
+            $('#' + dropdownName + ' li a:focus').click();
             return;
         }
     });
